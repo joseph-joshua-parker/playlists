0506fe4e7ca7146b2bdfddc06871d2cadc5b77a3
joseph-joshua-parker <joelnashjobs@yahoo.com>
2023-06-09 13:34:08 -0300

    Section 19: Dive Into Redux Toolkit

---
+++ src/store/index.js
@@ -0,0 +1,50 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const movieSlice = createSlice({
+	name: 'movie',
+	initialState:[],
+	reducers: {
+		addMovie(state, action){
+			state.push(action.payload);
+		},
+
+		removeMovie(state, action){
+			const index = state.indexOf(action.payload);
+			state.splice(index, 1);
+		},
+
+		reset(state, action){
+			return [];
+		}
+
+	}
+
+})
+
+const songSlice = createSlice({
+	name: 'song',
+	initialState:[],
+	reducers: {
+		addSong(state, action){
+			state.push(action.payload);
+		},
+
+		removeSong(state, action){
+			const index = state.indexOf(action.payload);
+			state.splice(index, 1);
+		}
+	}
+});
+
+const store = configureStore({
+	reducer: {
+		songs: songSlice.reducer,
+		movies: movieSlice.reducer
+	}
+});
+
+
+
+export {store};
+export const {addSong, removeSong} = songSlice.actions;
+export const {addMovie, removeMovie, reset} = movieSlice.actions;
